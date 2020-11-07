7a511de5761c03f44acd130705557d26855ef482
Julian Camilo <julian20141016@gmail.com>
2020-11-07 14:01:46 -0500

    Created 404 page

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Navbar, Nav, NavItem, NavLink } from "reactstrap";
+import { Link } from "react-router-dom";
+import iconNav from "../images/brand.png";
+
+const NavBarCollapse = (props) => {
+  return (
+    <div>
+      <Navbar
+        className="navbar-dark bg-dark"
+        light
+        style={{ marginBottom: "10px"}}
+      >
+        <Nav className="mr-auto" navbar style={{ minHeight: '60px'}}>
+          <NavItem>
+            <NavLink style={{position: 'absolute', left: '50%', transform: 'translatex(-50%)'}}>
+              <Link
+                to="/home/add"
+                className="text-white"
+                style={{ textDecoration: "none" }}
+              >
+                <img src={iconNav} className="img-fluid mx-auto d-block" style={{maxHeight: '50px'}}></img>
+              </Link>
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBarCollapse;
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,20 @@
 import React from "react";
+import NavBar from "./NavBar";
 import Image404 from "../images/404.jpg";
+import "./styles/NotFound.css"
 
 export default class NotFound extends React.Component {
   render() {
     return (
-      <div className="container">
-        <img src={Image404} alt="" className="img-fluid mx-auto d-block" style={{maxWidth: '80%'}}/>
+      <div>
+        <NavBar />
+        <div className="container">
+          <img
+            src={Image404}
+            alt=""
+            className="image404 mx-auto d-block"
+          />
+        </div>
       </div>
     );
   }
