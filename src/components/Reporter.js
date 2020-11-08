ed3ca8d7e098bd0f02c3e8ae2dc0eb75e52a385a
Julian Camilo <julian20141016@gmail.com>
2020-11-08 11:26:10 -0500

    Recording screen finished

--- src/components/Reporter.js
+++ src/components/Reporter.js
@@ -3,10 +3,21 @@ import NavBar from "./NavBar";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
+import { Typography } from "antd";
+import { Button } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMicrophone,
+  faDownload,
+  faBackspace,
+} from "@fortawesome/free-solid-svg-icons";
+const { Paragraph, Title } = Typography;
 
 const Reporter = () => {
   const [name, setName] = useState("");
+  const [visible, setVisible] = useState(true);
   const { transcript, resetTranscript } = useSpeechRecognition();
+  const [editableStr, setEditableStr] = useState("");
 
   useEffect(() => obtenerSesion(), []);
 
@@ -20,16 +31,69 @@ const Reporter = () => {
       setName(session.name);
       console.log(name);
     }
+    SpeechRecognition.startListening({ continuous: true, language: "es-CO" });
+  };
+
+  const pasarAEdicion = () => {
+    console.log("se pasa a edicion");
+    setEditableStr(transcript);
+    resetTranscript();
+    SpeechRecognition.stopListening();
+    setVisible(false);
+  };
+
+  const siteLayoutContent = {
+    minHeight: "280px",
+    padding: "24px",
+    background: "#fff",
+    maxWidth: "500px",
+    margin: "40px",
+    marginLeft: "auto",
+    marginRight: "auto",
   };
 
   return (
     <div>
+      {document.body.setAttribute("style", "background-color: #F0F2F5;")}
       <NavBar />
-      <div>El usuario logeado es: {name}</div>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <div style={siteLayoutContent}>
+        <Title level={2}>Bienvenido, {name}</Title>
+        <Paragraph style={{ marginTop: "-15px" }}>
+          <strong>Habla</strong> ahora o <strong>escribe</strong> para generar
+          tu informe diario
+        </Paragraph>
+        <hr />
+        {visible ? (
+          <Paragraph style={{ fontSize: "25px" }}>{transcript}</Paragraph>
+        ) : (
+          <Paragraph
+            editable={{ onChange: setEditableStr }}
+            style={{ fontSize: "25px" }}
+          >
+            {editableStr}
+          </Paragraph>
+        )}
+        <center>
+          <Button
+            color="danger"
+            style={{ borderRadius: "20px" }}
+            onClick={pasarAEdicion}
+          >
+            <FontAwesomeIcon icon={faMicrophone}></FontAwesomeIcon>
+            <strong> Guardar </strong>
+          </Button>
+          {"  "}
+          <Button
+            color="primary"
+            outline
+            style={{ borderRadius: "20px" }}
+            onClick={resetTranscript}
+          >
+            <FontAwesomeIcon icon={faBackspace}></FontAwesomeIcon>
+            <strong> Limpiar </strong>
+          </Button>
+        </center>
+      </div>
     </div>
   );
 };
