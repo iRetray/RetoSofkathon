e5b97faee88bfeed57e45d494e274c3f0034ba31
Julian Camilo <julian20141016@gmail.com>
2020-11-07 23:34:38 -0500

    Maked speechRecorder

--- src/components/Reporter.js
+++ src/components/Reporter.js
@@ -1,34 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
-export default class Reporter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-    };
-    this.obtenerSesion = this.obtenerSesion.bind(this)
+const Reporter = () => {
+  const [name, setName] = useState("");
+  const { transcript, resetTranscript } = useSpeechRecognition();
+
+  useEffect(() => obtenerSesion(), []);
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return null;
   }
 
-  componentDidMount() {
-    this.obtenerSesion();
-  }
-
-  obtenerSesion() {
+  const obtenerSesion = () => {
     if (localStorage.getItem("userSession")) {
       let session = JSON.parse(localStorage.getItem("userSession"));
-      this.setState({
-        name: session.name,
-      });
+      setName(session.name);
+      console.log(name);
     }
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <div>El usuario logeado es: {this.state.name}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <NavBar />
+      <div>El usuario logeado es: {name}</div>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
+};
+
+export default Reporter;
